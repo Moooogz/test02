9c0ac5c9be9c05b98f23c1fe39870e8642f2ce4a
moooogz <markdavedizon@yahoo.com>
2023-07-17 14:15:00 +0800

    Deployed website

--- test02/src/App.js
+++ test02/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import {BrowserRouter as Router, Routes,Route,Link} from 'react-router-dom'
 import Ssu from './Pages/Ssu';
 import Abca from './Pages/Abca';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
     <div className="App">
      
-      <Router>              
-     
-        <Link to="/">SSU</Link>
+      <Router>   
+              
+             
+             <Link href="#" to="/">SSU</Link>
         <Link to="/abca">ABCA</Link>       
         <Routes>
           <Route path="/" element={<Ssu />}/>
