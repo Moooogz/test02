3b8b45f233aff6206bce92040ed35899e848fdca
moooogz <markdavedizon@yahoo.com>
2023-07-17 13:05:31 +0800

    first commit

---
+++ test02/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <h1> Welcome SSU Members!</h1> -mooogz
+      </header>
+    </div>
+  );
+}
+
+export default App;
