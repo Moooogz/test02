7d9f311337e51c8132e99cbb71c1d7f56ac760b2
moooogz <markdavedizon@yahoo.com>
2023-07-18 09:21:47 +0800

    Deployed website

--- test02/src/components/login.js
+++ test02/src/components/login.js
@@ -7,22 +7,29 @@ const Login = () => {
     <Grid>
       <Paper
         elevation={10}
+        square={false}
+        rounded={100}
         style={
             { padding: 40,
              height: "50vh",
              width: 350,
-             margin: "60px auto" }
+             margin: "60px auto"
+           }
             }
       >
         <Grid align='center'>
           <Avatar style={
             {backgroundColor: '#126b83'}}>SSU</Avatar>
           <h1>Sign In</h1>
+          <Grid margin={"20px auto"}>
           <TextField id="filled-basic" label="Enter username" variant="filled" fullWidth margin ="10px"/>
+          </Grid>
           <Grid align='center'>
           <TextField id="filled-basic" label="Enter password" variant="filled" fullWidth  type="password"/>
           </Grid>
-          <Button variant="contained">Log in to SSU Website</Button>
+          <Grid margin={"20px auto"}>
+          <Button variant="contained" fullWidth>LOG IN</Button>
+          </Grid>
         </Grid>
       </Paper>
     </Grid>
